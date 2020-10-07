13a23e5fa38cb0665e684137855c4feb6db1ce1d
cwtk <zysaq@live.com>
2020-10-07 23:42:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,14 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+cd package/
+git clone https://github.com/kenzok8/openwrt-packages.git
+git clone https://github.com/kenzok8/small.git
+rm -rf small/ipt2socks
+rm -rf small/shadowsocksr-libev
+rm -rf small/pdnsd-alt
+rm -rf small/verysync
